1993e47b6bddfe30b46f00aaa03e41830c2ac21f
Jonathan <30177086+MonliH@users.noreply.github.com>
2020-07-10 14:35:45 -0400

    Added solution

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,77 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.history = [0]
+        self.history_position = 0
+
+    def _insert_history(self, change: int):
+        """Replace a value in the history if it exists, otherwise append"""
+        if len(self.history) > self.history_position:
+            self.history[self.history_position] = change
+        else:
+            self.history.append(change)
+
+        # Increment history position (we inserted an element)
+        self.history_position += 1
+
+    def _update_value(self, amount: int):
+        """Update value based on history position
+
+        Positional Arguments:
+        amount -- how far back into the history to travel. 
+
+        Notes:
+        * `amount` should be negative to travel backwards, positive to travel forwards
+        """
+        
+        
+        adjusted_idx = (self.history_position+amount, self.history_position)
+
+        # Sum all of the actions done between the range we want to undo/redo
+        undone_actions = sum(self.history[min(adjusted_idx):max(adjusted_idx)])
+        
+        self.value -= undone_actions if amount < 0 else -undone_actions
+        self.history_position += amount
 
     def add(self, num: int):
-        pass
+        """Add to value
+
+        Positional Arguments:
+        num -- value to add
+        """
+        self.value += num
+        self._insert_history(+num)
 
     def subtract(self, num: int):
-        pass
+        """Subtract from value
+
+        Positional Arguments:
+        num -- value to subtract
+        """
+        self.value -= num
+        self._insert_history(-num)
 
     def undo(self):
-        pass
+        """Undo one change to value"""
+        self._update_value(-1)
 
     def redo(self):
-        pass
+        """Redo one change to value"""
+        self._update_value(+1)
 
     def bulk_undo(self, steps: int):
-        pass
+        """Undo multiple changes to value
+
+        Positional Arguments:
+        steps -- amount of actions to undo
+        """
+        self._update_value(-steps)
 
     def bulk_redo(self, steps: int):
-        pass
+        """Redo multiple changes to value
+
+        Positional Arguments:
+        steps -- amount of actions to redo
+        """
+
+        self._update_value(+steps)
